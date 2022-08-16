385c87a08e37eeec49b07a6ba4df5514ef591614
Denis <black1t@yandex.ru>
2022-08-16 15:52:15 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import './src/style.css'
+import './src/normalize.css'
+import './src/header.css'
+import './src/info.css'
+import './src/directions.css'
+import './src/lessons.css' 
+import './src/videolesson.css' 
+import './src/reviews.css' 
+import './src/footer.css'
+
+
+
+import Splide from '@splidejs/splide';
+
+new Splide( '.splide' ).mount();
+
+
+
+
+
+
